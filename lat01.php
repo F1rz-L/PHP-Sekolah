3b33551ab23ee44a8f0755fec4a565bf5cbf6cfb
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-12-25 20:32:09 +0700

    Create lat01.php

---
+++ lat01.php
@@ -0,0 +1,26 @@
+<?php 
+
+    // echo "<h1>Aku keren banget</h1>";
+
+    // $tulisan = 'Saya belajar';
+    // echo $tulisan;
+    // $tulisan = 30;
+    // echo $tulisan . "Hai" . "<br>";
+    // var_dump($tulisan);
+    // echo "<br><br>";
+
+    // function belajar(){
+    //     echo "saya Keren" . 30;
+    // }
+    // belajar();
+
+    $rat = array(
+        "joni" => 1,
+        "Kono" => 2,
+        "Jeki" => 3
+    );
+
+    foreach ($rat as $key) {
+        echo $key;
+    }
+?>
