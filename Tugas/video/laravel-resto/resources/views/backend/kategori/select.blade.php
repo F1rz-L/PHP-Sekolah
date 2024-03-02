aad0db0b6efb12d766ebe75ded70fda2221fd070
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2024-03-02 16:41:18 +0700

    v7

--- Tugas/video/laravel-resto/resources/views/backend/kategori/select.blade.php
+++ Tugas/video/laravel-resto/resources/views/backend/kategori/select.blade.php
@@ -1,5 +1,35 @@
 @extends('/backend.back')
 
 @section('admincontent')
-    <h1>Kategori</h1>
+    <div class="">
+        <h1>Kategori</h1>
+    </div>
+    <div class="">
+        <a href="{{ url('/admin/kategori/create') }}" class="btn btn-success">Tambah Data</a>
+    </div>
+    <div class="">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Kategori</th>
+                    <th>Ubah</th>
+                    <th>Hapus</th>
+                </tr>
+            </thead>
+            @php
+                $no = 0;
+            @endphp
+            <tbody>
+                @foreach ($kategoris as $kategori)
+                    <tr>
+                        <td>{{ $no += 1 }}</td>
+                        <td>{{ $kategori->kategori }}</td>
+                        <td><a href="{{ url('/admin/kategori/'.$kategori->idkategori.'/edit') }}" class="btn btn-secondary">Ubah</a></td>
+                        <td><a href="{{ url('/admin/kategori/'.$kategori->idkategori) }}" class="btn btn-danger">Hapus</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
