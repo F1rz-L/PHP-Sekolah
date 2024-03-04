c9ae74bead0485cb96a1d12241a699b4ade392c0
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2024-03-04 21:37:03 +0700

    v55

--- Tugas/video/php/restoran/index.php
+++ Tugas/video/php/restoran/index.php
@@ -1,19 +1,18 @@
-<!-- <h1>Home Page</h1> -->
+<?php
+session_start();
+require_once "dbcontroller.php";
+$db = new DB;
 
-<?php 
-    session_start();
-    require_once "dbcontrol.php";
-    $db = new DB;
+$sql = "SELECT * FROM tblkategori ORDER BY kategori";
+$row = $db->getALL($sql);
 
-    $sql = "SELECT * FROM tblkategori ORDER BY kategori";
-    $row = $db -> getALL($sql);
+if (isset($_GET['log'])) {
+    session_destroy();
+    header("location:index.php");
+}
 
-    if (isset($_GET['log'])) {
-        session_destroy();
-        header("location:index.php");
-    }
+function cart(){
 
-    function cart() {
     global $db;
 
     $cart = 0;
@@ -23,7 +22,8 @@
             $id = substr($key, 1);
 
             $sql = "SELECT * FROM tblmenu WHERE idmenu=$id";
-            $row = $db -> getALL($sql);
+
+            $row = $db->getALL($sql);
 
             foreach ($row as $r) {
                 $cart++;
@@ -32,80 +32,93 @@
     }
     return $cart;
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Restoran SMK | Home Page</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Restoran SMK | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 </head>
+
 <body>
     <div class="container">
+
         <div class="row">
             <div class="col-md-3 mt-4">
-                <h2><a href="index.php" class="text-dark" style="text-decoration:none;">Home Page Restoran</a></h2>
+                <h2> <a href="index.php"> Restoran SMK </a></h2>
             </div>
+
             <div class="col-md-9">
-            <?php
+
+                <?php
                 if (isset($_SESSION['pelanggan'])) {
                     echo  ' 
                         <div class="float-end mt-4"><a href="?log=logout">Logout</a></div>
-                        <div class="float-end mt-4 me-4">Pelanggan : <a href="?f=home&m=beli">' . $_SESSION['pelanggan'] . ' </a> </div>
-                        <div class="float-end mt-4 me-4">Cart : ( <a href="?f=home&m=beli"> ' . cart() . ' </a> ) </div>
-                        <div class="float-end mt-4 me-4"> <a href="?f=home&m=histori"> Histori </a> </div>
-                        ';
+                        <div class="float-end mt-4 mr-4 me-4">Pelanggan : ' . $_SESSION['pelanggan'] . ' </div>
+                        <div class="float-end mt-4 mr-4 me-4">Cart : ( <a href="?f=home&m=beli"> ' . cart() . ' </a> ) </div>
+                        <div class="float-end mt-4 mr-4 me-4"> <a href="?f=home&m=histori"> Histori </a> </div>
+                    
+                    ';
                 } else {
                     echo '
-                        <div class="float-end mt-4 me-4"><a href="?f=home&m=login">Login</a></div>
-                        <div class="float-end mt-4 me-4"><a href="?f=home&m=daftar">Daftar</a></div>
+                        <div class="float-end mt-4 mr-4 me-4"><a href="?f=home&m=login">Login</a></div>
+                        <div class="float-end mt-4 mr-4 me-4"><a href="?f=home&m=daftar">Daftar</a></div>
 
                     ';
                 }
-            ?>
+                ?>
+
+
+
+
             </div>
-            
         </div>
 
-        <div class="row">
-        <div class="row mt-4">
+        <div class="row mt-5">
             <div class="col-md-3">
                 <h3>Kategori</h3>
                 <hr>
-                <?php if(!empty($row)) {?>
-                <ul class="nav flex-column">
-                    
-                    <?php foreach($row as $r): ?>
-                        <li class="nav-item"><a class="nav-link" href="?f=home&m=produk&id=<?php echo $r['idkategori'] ?>"><?php echo $r['kategori'] ?></a></li>
-                    <?php endforeach ?>
-                </ul>
-                <?php }?>
+                <?php if (!empty($row)) { ?>
+                    <ul class="nav flex-column">
+
+                        <?php foreach ($row as $r) : ?>
+                            <li class="nav-item"><a class="nav-link" href="?f=home&m=produk&id=<?php echo $r['idkategori'] ?>"><?php echo $r['kategori'] ?></a></li>
+                        <?php endforeach ?>
+                    </ul>
+                <?php } ?>
             </div>
 
             <div class="col-md-9">
-                <!-- <h1>Tes</h1> -->
-               <?php 
-                    if (isset($_GET['f']) && isset($_GET['m'])) {
-                        $f = $_GET['f'];
-                        $m = $_GET['m'];
-    
-                        $file = $f . '/' . $m . '.php';
-    
-                        require_once $file;
-                    } else {
-                        require_once "home/produk.php";
-                    }
+                <?php
+                if (isset($_GET['f']) && isset($_GET['m'])) {
+                    $f = $_GET['f'];
+                    $m = $_GET['m'];
+
+                    $file = $f . '/' . $m . '.php';
+
+                    require_once $file;
+
+                } 
+                else {
+                    require_once "home/produk.php";
+                }
                 ?>
             </div>
         </div>
 
-        <div class="row fixed-bottom">
+        <div class="row mt-5">
+
             <div class="col">
-            <p class="text-center">2024 - copyright@societyco.com</p>
+                <p class="text-center">2024 - copyright@firz.id</p>
             </div>
         </div>
     </div>
 </body>
+
 </html>
