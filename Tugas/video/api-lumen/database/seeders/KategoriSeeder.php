f9f3900036da3b47077879b43ee6ec149e521bbd
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2024-03-07 13:14:39 +0700

    lumen v7

---
+++ Tugas/video/api-lumen/database/seeders/KategoriSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory as Faker;
+use App\Models\Kategori;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Testing\Fakes\Fake;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            $data = [
+                'kategori' => $faker->citySuffix,
+                'keterangan' => $faker->text(20),
+            ];
+
+            Kategori::create($data);
+        }
+    }
+}
---
+++ Tugas/video/api-lumen/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group( ['prefix' => 'api'], function() use ($router) {
+    $router->get('/kategori', ['uses' => 'KategoriController@index']);
+    $router->get('/kategori/{id}', ['uses' => 'KategoriController@show']);
+    $router->post('/kategori', ['uses' => 'KategoriController@create']);
+    $router->put('/kategori/{id}', ['uses' => 'KategoriController@update']);
+    $router->delete('/kategori/{id}', ['uses' => 'KategoriController@destroy']);
+});
